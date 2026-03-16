1e02248ea6b6de48d58b2d6f8b63d0300d9e0b6d
fatima <242094@edu.fa.ru>
2026-03-16 16:53:13 +0300

    lab01 added

---
+++ lab01_tcp_echo_one/results.py
@@ -0,0 +1,8 @@
+fatima@fatima-VirtualBox:~/socket/lab01_tcp_echo_one$ python3 server.py
+server is listening in port 9090
+server is accepted new client ('127.0.0.1', 42620)
+client sent this data hello
+
+client.py
+fatima@fatima-VirtualBox:~/socket/lab01_tcp_echo_one$ python3 client.py
+echo hello
